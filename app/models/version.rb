a9a32d46833670ed85e2bb288e052986d4a06f6c
A R <ariaru@hotmail.de>
2015-07-15 17:49:32 +0200

    Add Active Record Associations to Version model

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,6 @@
 class Version < ActiveRecord::Base
   belongs_to :author
-  belongs_to :parent
+  belongs_to :parent, class_name: "Version"
+  has_one :document
+  has_many :children, class_name: "Version", foreign_key: "parent_id"
 end
