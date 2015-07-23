9069e89bd6f8dc890e89bf7d268650dc491843dc
A R <ariaru@hotmail.de>
2015-07-23 13:45:32 +0200

    Add version_params, flash messages, validations
    version_params: whitelist as private controller method, gets passed to create action
    flash messages: for success/error upon submission of new version data via web form (error if submitted data not valid, defined by validations (next)
    validations: simply for presence, so no blank forms can be submitted+saved to DB

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -4,5 +4,19 @@ class VersionsController < ApplicationController
   end
 
   def create
+    @version = Version.new(version_params)
+
+    if @version.save
+      flash[:success] = 'Version saved.'
+      redirect_to new_version_path
+    else
+      flash[:danger] = 'Error occurred, version has not been saved.'
+      redirect_to new_version_path
+    end
   end
+
+  private
+    def version_params
+      params.require(:version).permit(:summary, :body, :tags)
+    end
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -1,4 +1,6 @@
 class Version < ActiveRecord::Base
+  validates :summary, presence: true
+  validates :body, presence: true
   belongs_to :document
   belongs_to :user
   belongs_to :parent, inverse_of: :children, class_name: "Version"
