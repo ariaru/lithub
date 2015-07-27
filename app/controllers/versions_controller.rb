bdf00ad9efd2076beea6f8412a48a21a57ac0be5
A R <ariaru@hotmail.de>
2015-07-27 18:24:41 +0200

    Add update method to versions controller
    Add :parent_id to whitelist for version_params

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -22,8 +22,23 @@ class VersionsController < ApplicationController
   def show
   end
 
+  def edit
+    @version = Version.find(params[:id])
+  end
+
+  def update
+    @version = Version.new(version_params)
+    if @version.save
+      flash[:success] = 'Version saved.'
+      redirect_to new_version_path
+    else
+      flash[:danger] = 'Error occurred, version has not been saved.'
+      redirect_to new_version_path
+    end
+  end
+
   private
     def version_params
-      params.require(:version).permit(:summary, :body, :tags)
+      params.require(:version).permit(:summary, :body, :tags, :parent_id)
     end
 end
