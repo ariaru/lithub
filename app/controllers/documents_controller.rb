ecc1114036fa586e2ba6fe4b52f306f6d47f0494
A R <ariaru@hotmail.de>
2015-08-17 18:14:34 +0200

    Try to set up nested form fields for Doc, doesn't work yet

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -69,6 +69,6 @@ class DocumentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def document_params
-      params[:document]
+      params.require(:document).permit(:title, :branch)
     end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,5 @@ class Document < ActiveRecord::Base
   belongs_to :user
   belongs_to :repository
   has_many :branches
+  accepts_nested_attributes_for :branches, allow_destroy: true
 end
