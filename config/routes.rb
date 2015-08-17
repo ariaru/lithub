a7189c85db1bd6afcc7a31b4efe61d1c3c8fceca
A R <ariaru@hotmail.de>
2015-08-17 15:43:54 +0200

    Add nested resources Documents-Branches-Revisions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :revisions
+  resources :documents do
+    resources :branches do
+      resources :revisions
+    end
+  end
   get '/about' => 'pages#about'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
