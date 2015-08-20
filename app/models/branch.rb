8b1f973dc86e847f09cae74e7f9d359142f1c880
A R <ariaru@hotmail.de>
2015-08-20 17:09:50 +0200

    Update models

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,6 +1,10 @@
 class Branch < ActiveRecord::Base
+  belongs_to :repository
   has_many :documents, dependent: :destroy
+  has_many :revisions, dependent: :destroy
   accepts_nested_attributes_for :documents, reject_if: proc { |attr| attr[:title].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :revisions, reject_if: proc { |attr| attr[:summary, :body].blank? }, allow_destroy: true
+
   validates :name, presence: true
 
   def to_s
--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,10 +1,12 @@
 class Revision < ActiveRecord::Base
+  belongs_to :branch
+  belongs_to :document
+  belongs_to :parent, inverse_of: :children, class_name: "Revision"
+  has_many :children, inverse_of: :parent, class_name: "Revision", foreign_key: "parent_id"
+
   validates :summary, presence: true
   validates :body, presence: true
-  belongs_to :document
-  has_many :children, inverse_of: :parent, class_name: "Revision", foreign_key: "parent_id"
-  belongs_to :parent, inverse_of: :children, class_name: "Revision"
-
+  
   before_save :reassign_parent_and_delete_obsolete_drafts_if_necessary
 
   protected
